b8fd3efe5d18ebe0359b536a768f14b8bae6210c
MinSoo <minsu_510@naver.com>
2024-09-15 21:19:23 +0900

    first commit

--- src/DataType.kt
+++ src/DataType.kt
@@ -1,2 +1,44 @@
-class DataType {
+// 자료형
+// Data Type
+
+fun main(args: Array<String>) {
+    // Byte
+    var mByte : Byte = 100
+
+    // Short
+    var mShort : Short = 1000
+
+    // Int
+    var mInt : Int = 100000
+    // 회색 처리되는 이유는 자료형을 써주지 않아도 알고있다는 뜻이다.
+    // 왜냐? 기본형은 Int 이기 때문이다.
+
+    // Long
+    var mLong : Long = 100000000L
+
+    // Float
+    var mFloat : Float = 1.53f
+
+    // Double
+    var mDouble : Double = 1.2654576
+
+    // Characters
+    var mChar : Char = 'c' // 글자 하나만
+    // 그리고 작은 따옴표를 감지한 순간 character 로 감지한다.
+    // String 도 마찬가지
+
+    // String
+    var mString : String = "Hello Word!"
+    // Char 는 '작은 따옴표', String 은 "큰 따옴표"
+
+    // Booleans
+    var mFalse : Boolean = false
+    var mTrue : Boolean = true
+    // 참, 거짓
+
+    // Arrays
+    var mArray : Array<String> = arrayOf("Kotlin", "Is", "Fun") // 0, 1, 2
+    println(mArray[0])
+    println(mArray.get(1)) // 둘 다 같은거다.
+    println(mArray.size)
 }
--- src/Variable.kt
+++ src/Variable.kt
@@ -1,2 +1,12 @@
-class Variable {
+fun main(args: Array<String>) {
+    // 변수와 상수
+    val pi : Double = 3.14 // 상수를 지정, pi 라는 이름, double 타입, 값은 3.14할당
+    // 상수는 변하지 않는 값이다.
+    println(pi)
+
+    // variable
+    var age : Int = 26
+    println(age)
+    age = 27
+    println(age) // 재할당
 }
